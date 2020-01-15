cfbdd6ddbc3ff45f7733d058af2de3e1181d0f67
jairofilho79 <jairofilho79@gmail.com>
2020-01-15 02:22:58 -0300

    Entire Automation Test

---
+++ notFound.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const screenSizeWidth = 1920;
+    const screenSizeHeight = 937;
+    const browser = await puppeteer.launch({
+        // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+        headless: false,
+        args: [`--window-size=${screenSizeWidth},${screenSizeHeight}`]
+    });
+    const pages = await browser.pages();
+
+    const page = pages[0];
+    const override = Object.assign(page.viewport(), {width: screenSizeWidth ,height: screenSizeHeight});
+    await page.setViewport(override);
+
+    // Open page.
+    await page.goto('http://localhost:4200');
+
+    // Focus input field.
+    await page.waitForSelector('#searchInput');
+    await page.type('#searchInput','Vingadores', {delay: 20});
+    await page.keyboard.press(String.fromCharCode(13)); //Enter
+
+    await page.waitFor(3000);
+    await browser.close();
+
+})();
